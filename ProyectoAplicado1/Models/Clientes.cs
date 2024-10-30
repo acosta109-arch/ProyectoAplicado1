94f9249bf54db248600e5d2beb018cbb94725855
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-10-30 00:44:38 -0400

    Se arreglo la identacion del RegularExpression del Modelo Clientes.

--- ProyectoAplicado1/Models/Clientes.cs
+++ ProyectoAplicado1/Models/Clientes.cs
@@ -8,23 +8,21 @@ namespace ProyectoAplicado1.Models
 		[Key]
 		public int ClienteId { get; set; }
 
+		[Required(ErrorMessage = "El nombre es obligatorio")]
+		[RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "El nombre solo puede contener letras y espacios")]
 		public string? Nombre { get; set; }
 
-        [Required(ErrorMessage = "El nombre es obligatorio")]
-        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "El nombre solo puede contener letras y espacios")]
-        public string? Apellido { get; set; }
+		[Required(ErrorMessage = "El Apellido es obligatorio")]
+		[RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "El nombre solo puede contener letras y espacios")]
+		public string? Apellido { get; set; }
 
-        [Required(ErrorMessage = "El Apellido es obligatorio")]
-        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "El nombre solo puede contener letras y espacios")]
-        public string? Telefono { get; set; }
+		[Required(ErrorMessage = "El Telefono es obligatorio")]
+		[RegularExpression(@"^\d{10}$", ErrorMessage = "El teléfono debe contener 10 dígitos")]
+		public string? Telefono { get; set; }
 
-        [Required(ErrorMessage = "El Telefono es obligatorio")]
-        [RegularExpression(@"^\d{10}$", ErrorMessage = "El teléfono debe contener 10 dígitos")]
-
-        public string? Correo { get; set; }
-
-        [Required(ErrorMessage = "El correo es obligatorio")]
-        [EmailAddress(ErrorMessage = "Por favor ingrese una dirección de correo válida")]
+		[Required(ErrorMessage = "El correo es obligatorio")]
+		[EmailAddress(ErrorMessage = "Por favor ingrese una dirección de correo válida")]
+		public string? Correo { get; set; }
 
         [ForeignKey("TarjetaId")]
 		public Tarjetas Tarjetas { get; set; }
