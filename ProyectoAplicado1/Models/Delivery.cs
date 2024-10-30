c79bf34498d33ceb884d950116a6b687e77fe4e5
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-10-30 00:45:04 -0400

    Se agregaron las expresiones regulares de Delivery.

--- ProyectoAplicado1/Models/Delivery.cs
+++ ProyectoAplicado1/Models/Delivery.cs
@@ -8,13 +8,21 @@ namespace ProyectoAplicado1.Models
 		[Key]
 		public int DeliveryId { get; set; }
 
+		[Required(ErrorMessage = "El nombre es obligatorio")]
+		[RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "El nombre solo puede contener letras y espacios")]
 		public string? Nombre { get; set; }
 
-		public string Apellido { get; set; }
+		[Required(ErrorMessage = "El Apellido es obligatorio")]
+		[RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "El nombre solo puede contener letras y espacios")]
+		public string? Apellido { get; set; }
 
-		public string Telefono { get; set; }
+		[Required(ErrorMessage = "El Telefono es obligatorio")]
+		[RegularExpression(@"^\d{10}$", ErrorMessage = "El teléfono debe contener 10 dígitos")]
+		public string? Telefono { get; set; }
 
-		public string Correo { get; set; }
+		[Required(ErrorMessage = "El correo es obligatorio")]
+		[EmailAddress(ErrorMessage = "Por favor ingrese una dirección de correo válida")]
+		public string? Correo { get; set; }
 
 		[ForeignKey("PedidoId")]
 		public Pedidos Pedidos { get; set; }
