6af5a14b632afd066d30e2fbea0657e8c3c29a06
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-10-29 23:04:21 -0400

    Se creo el Modelo Tarjetas.

---
+++ ProyectoAplicado1/Models/Tarjetas.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoAplicado1.Models
+{
+	public class Tarjetas
+	{
+		[Key]
+		public int TarjetaId { get; set; }
+
+		public int NumeroTarjeta { get; set; }
+
+		public int CVV { get; set; }
+
+		//Tipo
+
+		public DateOnly FechaVencimiento { get; set; }
+
+		public string Propietario { get; set; }
+	}
+}
