e98715a1eb44a465d35efa99ae0203b02b40c078
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-10-29 23:03:47 -0400

    Se creo el Modelo MetodoPagos.

---
+++ ProyectoAplicado1/Models/MetodoPagos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoAplicado1.Models
+{
+	public class MetodoPagos
+	{
+		[Key]
+		public int MetodoPagoId { get; set; }
+
+		public float Monto { get; set; }
+
+		[ForeignKey("TarjetaId")]
+		public Tarjetas tarjetas { get; set; }
+	}
+}
