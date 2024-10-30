fd19e37148e1811cd97ece8f1103a1ba7014fee2
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-10-29 23:03:32 -0400

    Se creo el Modelo Direcciones.

---
+++ ProyectoAplicado1/Models/Direcciones.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoAplicado1.Models
+{
+	public class Direcciones
+	{
+		[Key]
+		public int DireccionId { get; set; }
+
+		public string Descripcion { get; set; }
+
+		public string? Notas { get; set; }
+	}
+}
