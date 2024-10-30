aeb5ba56b75b807d87618c96be806b8dd2b80ac6
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-10-29 23:03:03 -0400

    Se creo el modelo Clientes.

---
+++ ProyectoAplicado1/Models/Clientes.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoAplicado1.Models
+{
+	public class Clientes
+	{
+		[Key]
+		public int ClienteId { get; set; }
+
+		public string Clave { get; set; }
+
+		public string? Nombre { get; set; }
+
+		public string Apellido { get; set; }
+
+		public string Telefono { get; set; }
+
+		public string Correo { get; set; }
+
+		[ForeignKey("TarjetaId")]
+		public Tarjetas Tarjetas { get; set; }
+
+		[ForeignKey("DireccionId")]
+		public Direcciones Direcciones { get; set; }
+	}
+}
