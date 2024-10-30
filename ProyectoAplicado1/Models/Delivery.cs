94949e2709dd989c0f11bc24f302420819e19320
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-10-29 23:03:23 -0400

    Se creo el Modelo Delivery.

---
+++ ProyectoAplicado1/Models/Delivery.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoAplicado1.Models
+{
+	public class Delivery
+	{
+		[Key]
+		public int DeliveryId { get; set; }
+
+		public string? Nombre { get; set; }
+
+		public string Apellido { get; set; }
+
+		public string Telefono { get; set; }
+
+		public string Correo { get; set; }
+
+		[ForeignKey("PedidoId")]
+		public Pedidos Pedidos { get; set; }
+	}
+}
