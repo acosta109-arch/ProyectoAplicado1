af5d7d09944b41ae6ef0839a4392f26eb060cbc4
MarianVentura <marianv1119@gmail.com>
2024-11-05 22:24:57 -0400

    Cree el modelo de CarritoItem

---
+++ ProyectoAplicado1/Models/CarritoItem.cs
@@ -0,0 +1,37 @@
+﻿using ProyectoAplicado.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoAplicado1.Models;
+
+public class CarritoItem
+{
+    [Key]
+    public int ArticuloId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Precio { get; set; }
+
+    public decimal Subtotal => Cantidad * Precio;
+
+    [ForeignKey("Comidas")]
+    public int? ComidaId { get; set; }
+
+    [ForeignKey("Bebidas")]
+    public int? BebidaId { get; set; }
+
+    [ForeignKey("PostreId")]
+    public int? PostreId { get; set; }
+
+    public Comidas Comida { get; set; }
+    public Bebidas Bebida { get; set; }
+    public Postres Postre { get; set; }
+
+    public ArticuloTipo TipoArticulo {  get; set; }
+
+    public enum ArticuloTipo
+    {
+        Comida,
+        Bebida,
+        Postre
+    }
+}
