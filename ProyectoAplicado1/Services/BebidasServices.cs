d28cee8cbb80f0f6ee14b3cab37bbfe95a95e3b7
MarianVentura <marianv1119@gmail.com>
2024-11-07 20:37:16 -0400

    Modificacion del servicio de bebidas

--- ProyectoAplicado1/Services/BebidasServices.cs
+++ ProyectoAplicado1/Services/BebidasServices.cs
@@ -1,78 +1,83 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProyectoAplicado.Models;
 using ProyectoAplicado1.Data;
+using ProyectoAplicado1.Models;
 using System.Linq.Expressions;
 
-namespace ProyectoAplicado.Services
+namespace ProyectoAplicado.Services;
+
+public class BebidasServices
 {
-    public class BebidasServices
+    private readonly ApplicationDbContext _contexto;
+
+    public BebidasServices(ApplicationDbContext contexto)
     {
-        private readonly ApplicationDbContext _contexto;
+        _contexto = contexto;
+    }
 
-        public BebidasServices(ApplicationDbContext contexto)
+    public async Task<bool> Existe(int bebidaId)
+    {
+        return await _contexto.Bebidas.AnyAsync(b => b.BebidaId == bebidaId);
+    }
+
+    public async Task<bool> Insertar(Bebidas bebida)
+    {
+        _contexto.Add(bebida);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Modificar(Bebidas bebida)
+    {
+        _contexto.Update(bebida);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        var eliminar = await _contexto.Bebidas
+        .Where(b => b.BebidaId == id).ExecuteDeleteAsync();
+        return eliminar > 0;
+    }
+
+    public async Task<Bebidas?> Buscar(int bebidaId)
+    {
+        return await _contexto.Bebidas
+            .AsNoTracking()
+            .FirstOrDefaultAsync(b => b.BebidaId == bebidaId);
+    }
+
+    public async Task<List<Bebidas>> Listar(Expression<Func<Bebidas, bool>> criterio)
+    {
+        return _contexto.Bebidas
+            .AsNoTracking()
+            .Where(criterio)
+            .ToList();
+    }
+
+    public async Task<bool> Guardar(Bebidas bebida)
+    {
+        if (await Existe(bebida.BebidaId))
         {
-            _contexto = contexto;
+            return await Modificar(bebida);
         }
-
-        public async Task<bool> Existe(int bebidaId)
+        else
         {
-            return await _contexto.Bebidas.AnyAsync(b => b.BebidaId == bebidaId);
+            return await Insertar(bebida);
         }
+    }
 
-        public async Task<bool> Insertar(Bebidas bebida)
-        {
-            _contexto.Add(bebida);
-            return await _contexto.SaveChangesAsync() > 0;
-        }
+    public async Task<bool> ValidarBebidaUnica(int bebidaId, string nombre, string descripcion)
+    {
+        var bebidaExistente = await _contexto.Bebidas
+            .Where(b => b.BebidaId != bebidaId)
+            .Where(b => b.Nombre == nombre || b.Descripcion == descripcion)
+            .FirstOrDefaultAsync();
 
-        public async Task<bool> Modificar(Bebidas bebida)
-        {
-            _contexto.Update(bebida);
-            return await _contexto.SaveChangesAsync() > 0;
-        }
+        return bebidaExistente == null;
+    }
 
-        public async Task<bool> Eliminar(int id)
-        {
-            var eliminar = await _contexto.Bebidas
-            .Where(b => b.BebidaId == id).ExecuteDeleteAsync();
-            return eliminar > 0;
-        }
-
-        public async Task<Bebidas?> Buscar(int bebidaId)
-        {
-            return await _contexto.Bebidas
-                .AsNoTracking()
-                .FirstOrDefaultAsync(b => b.BebidaId == bebidaId);
-        }
-
-        public async Task<List<Bebidas>> Listar(Expression<Func<Bebidas, bool>> criterio)
-        {
-            return _contexto.Bebidas
-                .AsNoTracking()
-                .Where(criterio)
-                .ToList();
-        }
-
-        public async Task<bool> Guardar(Bebidas bebida)
-        {
-            if (await Existe(bebida.BebidaId))
-            {
-                return await Modificar(bebida);
-            }
-            else
-            {
-                return await Insertar(bebida);
-            }
-        }
-
-        public async Task<bool> ValidarBebidaUnica(int bebidaId, string nombre, string descripcion)
-        {
-            var bebidaExistente = await _contexto.Bebidas
-                .Where(b => b.BebidaId != bebidaId)
-                .Where(b => b.Nombre == nombre || b.Descripcion == descripcion)
-                .FirstOrDefaultAsync();
-
-            return bebidaExistente == null;
-        }
+    public async Task<Item?> ObtenerPorId(int itemId)
+    {
+        return await _contexto.Items.FindAsync(itemId);
     }
 }
