7dfcdeae98e235a71968955b583ae9a5e5ae895c
Cape23180909 <samilgamer@outlook.com>
2024-11-20 23:55:48 -0400

    Terminando el modelo de reportes

---
+++ ProyectoAplicado1/Models/Reportes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoAplicado1.Models;
+
+public class Reportes
+{
+    [Key]
+    public int ReporteId { get; set; }
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo puede contener letras.")]
+    public string Nombre { get; set; }
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "La descripción solo puede contener letras.")]
+    public string Descripcion { get; set; }
+}
