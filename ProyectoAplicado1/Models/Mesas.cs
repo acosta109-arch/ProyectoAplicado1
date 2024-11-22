35315777de3c592fefabcf1dce8cc3edf9ce998a
ralfy <ralfy@RALFY>
2024-11-22 10:02:53 -0300

    agregando el modelo de mesas

---
+++ ProyectoAplicado1/Models/Mesas.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoAplicado1.Models;
+
+public class Mesas
+{
+    [Key]
+    public int MesaId { get; set; }
+
+    [Required(ErrorMessage = "Favor llenar el campo Nombre")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo puede contener letras.")]
+    public string Nombre { get; set; }
+
+}
