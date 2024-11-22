a63ed7e5203909336be4b63c8538947d3a23ccbe
ralfy <ralfy@RALFY>
2024-11-22 10:02:15 -0300

    agregando el DbContext de mesas

--- ProyectoAplicado1/Data/ApplicationDbContext.cs
+++ ProyectoAplicado1/Data/ApplicationDbContext.cs
@@ -19,6 +19,7 @@ namespace ProyectoAplicado1.Data
 
         public DbSet<OrdenesDelivery> OrdenesDelivery { get; set; }
         public DbSet<Reportes> Reportes { get; set; }
+        public DbSet<Mesas> Mesas { get; set; }
     }
 }
           
